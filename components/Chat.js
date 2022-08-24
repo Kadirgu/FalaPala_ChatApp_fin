41da7e0e8ef4e9e0592ce88e8efc33fa04c3da2c
Kadirgu <Kadirgu@users.noreply.github.com>
2022-08-24 10:11:19 +0200

    fin version

---
+++ components/Chat.js
@@ -0,0 +1,274 @@
+
+
+
+import React from 'react';
+import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble, InputToolbar} from 'react-native-gifted-chat'
+
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+
+import CustomActions from './CustomActions';
+
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
+
+import firebase from "firebase";
+import "firebase/firestore";
+
+import NetInfo from '@react-native-community/netinfo';
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+
+
+export default class Chat extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      messages: [],
+      uid: 0,
+   
+      user: {
+        _id: '',
+        name: '',
+      },
+isConnected: null,
+image: null,
+location: null
+
+    };
+
+
+
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNHECUKbWKGL0shCgBK-HN7ry6At3o504",
+  authDomain: "falapala-chatapp.firebaseapp.com",
+  projectId: "falapala-chatapp",
+  storageBucket: "falapala-chatapp.appspot.com",
+  messagingSenderId: "45406076426",
+  appId: "1:45406076426:web:b8808cc072f194755ae9f3",
+};
+
+if (!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+// Reference to the Firestore collection "messages"
+this.referenceChatMessages = firebase.firestore().collection("messages");
+
+}
+
+componentDidMount() {
+  // Set name as title chat
+  let { name } = this.props.route.params;
+  this.props.navigation.setOptions({ title: name });
+
+  // Check if user is offline or online
+  NetInfo.fetch().then((connection) => {
+    if (connection.isConnected) {
+      this.setState({
+        isConnected: true,
+      });
+
+      // Reference to load messages from Firebase
+      this.referenceChatMessages = firebase
+        .firestore()
+        .collection('messages');
+
+      // Authenticate user anonymously
+      this.authUnsubscribe = firebase
+        .auth()
+        .onAuthStateChanged(async (user) => {
+          if (!user) {
+            firebase.auth().signInAnonymously();
+          }
+          this.setState({
+            uid: user.uid,
+            messages: [],
+            user: {
+              _id: user.uid,
+              name: name,
+            },
+          });
+          this.unsubscribe = this.referenceChatMessages
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(this.onCollectionUpdate);
+        });
+    } else {
+      this.setState({
+        isConnected: false,
+      });
+      this.getMessages();
+    }
+  });
+}
+
+componentWillUnmount() {
+  if (this.isConnected) {
+    this.unsubscribe();
+    this.authUnsubscribe();
+  }
+}
+
+
+onCollectionUpdate = (querySnapshot) => {
+  const messages = [];
+  // go through each document
+  querySnapshot.forEach((doc) => {
+    // get the QueryDocumentSnapshot's data
+    let data = doc.data();
+    messages.push({
+      _id: data._id,
+      text: data.text,
+      createdAt: data.createdAt.toDate(),
+      user: {
+        _id: data.user._id,
+        name: data.user.name,
+      },
+      image: data.image || null,
+      location: data.location || null,
+    });
+  });
+  this.setState({
+    messages,
+  });
+};
+
+
+async getMessages() {
+  let messages = '';
+  try {
+    messages = await AsyncStorage.getItem('messages') || [];
+    this.setState({
+      messages: JSON.parse(messages)
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+
+
+async saveMessages() {
+  try {
+    await AsyncStorage.setItem('messages', JSON.stringify(this.state.messages));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+
+// delete function for testing
+async deleteMessages() {
+  try {
+    await AsyncStorage.removeItem('messages');
+    this.setState({
+      messages: []
+    })
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+
+
+
+// Add messages to Firebase 
+addMessages(message) {
+  this.referenceChatMessages.add({
+    uid: this.state.uid,
+    _id: message._id,
+    text: message.text || '',
+    createdAt: message.createdAt,
+    user: message.user,
+    image: message.image || null,
+    location: message.location || null,
+
+  });
+}
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages), }), () => { 
+        this.addMessages(this.state.messages[0]);
+        this.saveMessages()
+        this.deleteMessages()
+    })
+ 
+  }
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
+  }
+
+
+  renderInputToolbar(props) {
+    if (this.state.isConnected == false) {
+    } else {
+      return(
+        <InputToolbar
+        {...props}
+        />
+      );
+    }
+  }
+
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
+
+
+  renderCustomActions = (props) => {
+    return <CustomActions {...props} />;
+  };
+  render() {
+
+     let  {bgColor} = this.props.route.params;
+    return (
+    
+      <View style={{flex:1, justifyContent: 'center', backgroundColor: bgColor}}>
+        <GiftedChat
+            renderInputToolbar={this.renderInputToolbar.bind(this)}
+            renderActions={this.renderCustomActions}
+         renderBubble={this.renderBubble.bind(this)}
+         renderCustomView={this.renderCustomView}
+  messages={this.state.messages}
+  onSend={messages => this.onSend(messages)}
+  user={{ _id: this.state.user._id, name: this.state.user.name }}
+/>
+{ Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+ }
+</View>
+     
+    )
+  }
+}
---
+++ components/Start.js
@@ -0,0 +1,265 @@
+
+
+
+
+// import react native gesture handler
+import 'react-native-gesture-handler';
+
+
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Pressable, TouchableOpacity, ImageBackground,
+ } from 'react-native';
+
+import BackgroundImage from "../assets/BackgroundImage.png";
+import { Directions } from 'react-native-gesture-handler';
+
+
+export default class Start extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      name: '', 
+      bgColor: this.colors.blue,
+
+    };
+  }
+
+ 
+
+  colors = {
+    red: "#890000",
+    yellow: "#FFFF00",
+    blue: "#1B70A0",
+    green: "#1DA01B",
+  };
+
+  setBgColor = (color) =>    this.setState({ bgColor: color });
+ 
+ 
+  render() {
+    return (
+      <View style={  styles.container}>
+        <ImageBackground source={BackgroundImage} resizeMode="cover" style={styles.image}>
+        <View style={styles.titleFrame}>
+          <Text style={styles.title}>ChatPro</Text>
+              </View>
+
+              <View style={styles.box}>
+              <View style={styles.inputBox}>
+       <TextInput
+         style={styles.TextInput}
+         onChangeText={(name) => this.setState({name})}
+         value={this.state.name}
+         placeholder='Your name'
+       />
+      </View>
+
+
+               <View style={styles.chooseBox}>
+       <Text style={styles.chooseTtitle}>Choose Background Color</Text>
+       </View>
+       <View style={styles.colorFrame}>
+     
+       <TouchableOpacity
+       accessible={true}
+       accessibilityLabel="Choose color"
+       accessibilityHint="Let’s you choose  color green as background"
+       accessibilityRole="button"
+        style={styles.color1}
+  onPress={() => 
+    this.setBgColor(this.colors.green)
+  }
+>
+</TouchableOpacity>
+
+<TouchableOpacity
+accessible={true}
+accessibilityLabel="Choose color"
+accessibilityHint="Let’s you choose  color red as background"
+accessibilityRole="button"
+ style={styles.color2}
+  onPress={() => 
+    this.setBgColor(this.colors.red)
+  }
+>
+</TouchableOpacity>
+
+<TouchableOpacity
+accessible={true}
+accessibilityLabel="Choose color"
+accessibilityHint="Let’s you choose  color yellow as background"
+accessibilityRole="button"
+ style={styles.color3}
+  onPress={() => 
+    this.setBgColor(this.colors.yellow)
+  }
+>
+</TouchableOpacity>
+
+<TouchableOpacity
+accessible={true}
+accessibilityLabel="Choose color"
+accessibilityHint="Let’s you choose  color blue as background"
+accessibilityRole="button"
+style={styles.color4}
+  onPress={() => 
+    this.setBgColor(this.colors.blue)
+  }
+>
+</TouchableOpacity>
+</View>
+
+<Pressable style={styles.button}
+        
+          onPress={() => this.props.navigation.navigate('Chat', { 
+            name: this.state.name,
+            bgColor: this.state.bgColor
+           })}
+        >
+  <Text style={styles.pressText}>Start Chatting</Text>
+</Pressable>
+</View>
+
+
+</ImageBackground>
+      </View>
+    )
+  }
+  
+}
+
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+ chooseBox: {
+  flex:0.4,
+    marginRight: "auto",
+    paddingLeft: 15,
+    width: "88%",
+
+ 
+  },
+
+  chooseTtitle: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    opacity: 1,
+    marginBottom: 10,
+  },
+
+  box: {
+ 
+    height: "44%",
+    width: "88%",
+    marginBottom: 30,
+    backgroundColor: "#ffffff",
+    flexGrow: 1,
+    flexShrink: 0,
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderRadius: 10,
+    height: 260,
+    minHeight: 260,
+    maxHeight: 300,
+  },
+  button: {
+    width: "88%",
+    height: 70,
+    borderRadius: 8,
+    backgroundColor: "#757083",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pressText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  title : {
+    fontSize: 45,
+    fontWeight: "600",
+    color: "#FFFFFF",
+  },
+  titleFrame : {
+    width: "60%",
+    height: "auto",
+    alignItems: "center",
+    marginTop: 50,
+    resizeMode: "contain",
+    flex: 1,
+  },
+
+  inputBox: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    backgroundColor: "#ffffff",
+    borderWidth: 1,
+    borderColor: "#000",
+    height: 50,
+    width: '88%',
+    borderRadius: 5,
+    marginBottom: 40,
+    padding:10,
+  },
+  TextInput: {
+    marginTop:5,
+    fontSize: 16,
+    fontWeight: '300',
+    opacity: .5,
+    color: "#888",
+
+  },
+ 
+
+  colorFrame: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+color1: {
+  backgroundColor: "green",
+  padding: 10,
+  width: 50,
+  height: 50,
+  borderRadius:25
+},
+color2: {
+  backgroundColor: "red",
+  padding: 10,
+  width: 50,
+  height: 50,
+  borderRadius:25
+},
+color3: {
+  backgroundColor: "yellow",
+  padding: 10,
+  width: 50,
+  height: 50,
+  borderRadius:25
+},
+color4: {
+  backgroundColor: "blue",
+  padding: 10,
+  width: 50,
+  height: 50,
+  borderRadius:25
+},
+
+
+ 
+});
